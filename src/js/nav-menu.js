9d54e04ed5cdb4ad3c23c7627b8475524c7eabb4
Paul Rose <prose100@hotmail.com>
2016-01-20 13:37:00 -0600

    Created framework for index.html and nav-menu.js files.

--- src/js/nav-menu.js
+++ src/js/nav-menu.js
@@ -1,22 +1,24 @@
+;(function($){
 
-$(document).ready(function(){
-    // fade in and fade out
-    $(function () {
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
-                $('#uparrow').fadeIn();
-            } else {
-                $('#uparrow').fadeOut();
-            }
-        });
- 
-        // scroll body to 0px on click
-        $('#uparrow').click(function () {
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
-    });
- 
-});
+    var defaults = {
+
+    };
+
+    function MobileMenu (element, options) {
+        settings = $.extend({}, defaults, options);
+        $this = $(element);
+
+        this.init();
+    }
+
+    MobileMenu.prototype.init = function() {
+
+    }
+
+    $.fn.mobilemenu = function(options) {
+        return this.each(function() {
+            new MobileMenu(this, options);
+        })
+    }
+
+})(jQuery);
