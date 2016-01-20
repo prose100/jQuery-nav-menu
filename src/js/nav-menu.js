96583844d27d7360d4ad28f517dad8047c2e904b
Paul Rose <prose100@hotmail.com>
2016-01-20 15:12:43 -0600

    Created burger.

--- src/js/nav-menu.js
+++ src/js/nav-menu.js
@@ -9,10 +9,16 @@
         $this = $(element);
 
         this.init();
+        console.log('hi');
     }
 
     MobileMenu.prototype.init = function() {
+        this.createBurger();
+    }
 
+    MobileMenu.prototype.createBurger = function() {
+        var $navToggle = $('<a>').attr('id', 'navToggle').insertAfter($this);
+        var $span = $('<span>').appendTo($navToggle);
     }
 
     $.fn.mobilemenu = function(options) {
