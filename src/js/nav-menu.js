b2d80b5106d6ae72240b8f072d367a4eda020727
Paul Rose <prose100@hotmail.com>
2016-01-21 23:22:45 -0600

    Created and animated sidebar.

--- src/js/nav-menu.js
+++ src/js/nav-menu.js
@@ -1,33 +1,57 @@
 ;(function($){
 
     var defaults = {
-        windowWidth: "800"
+        windowWidth: "800",
+        duration: 500,
+        sidebarLocation: 'left'
     };
 
     function MobileMenu (element, options) {
         settings = $.extend({}, defaults, options);
         $this = $(element);
 
+        this.isMobile = false;
         this.init();
     }
 
     MobileMenu.prototype.init = function() {
+
         var $burger = this.createBurger();
 
-        console.log($burger);
-        this.display($burger);
+        this.initDisplay($burger);
         this.resizeWindow($burger);
         this.animateBurger($burger);
+
+        var $sidebar = this.createSidebar();
+        var $body = $('<body>');
+
+        this.animate($burger, $sidebar, $body);
+    }
+
+    MobileMenu.prototype.toggleMobile = function() {
+        this.isMobile = !this.isMobile;
     }
 
     MobileMenu.prototype.createBurger = function() {
-        var $burger = $('<a>').attr('id', 'burger').insertAfter($this);
+        var $burger = $('<div>').attr('id', 'burger').insertAfter($this);
         var $span = $('<span>').appendTo($burger);
 
         return $burger;
     }
 
-    MobileMenu.prototype.display = function($burger) {
+    MobileMenu.prototype.createSidebar = function() {
+        var $sidebar = $('<ul>').attr('id', 'sidebar').insertAfter($this);
+        var $sidebarList = $this.children().css({}).clone().appendTo($sidebar);
+
+        $sidebar.css({'position':'absolute', 'list-style':'none',
+                        'display':'inline-block', 'padding': 0});
+        $sidebar.css(this.positionInit($sidebar));
+
+        console.log(-$sidebar.width());
+        return $sidebar;
+    }
+
+    MobileMenu.prototype.initDisplay = function($burger) {
         if ($(window).width()<settings.windowWidth) {
                 $burger.show();
                 $this.hide();
@@ -41,7 +65,7 @@
         var _this = this;
 
         $(window).resize(function() {
-           _this.display($burger);
+           _this.initDisplay($burger);
         })
     }
     
@@ -49,6 +73,42 @@
         $burger.click(function() {
             $(this).toggleClass('active');
             });
+    }
+
+    MobileMenu.prototype.positionInit = function($sidebar) {
+        if (settings.sidebarLocation == "left") {
+            return {left:-$sidebar.width(), top:0};
+        } else {
+            return {right:-$sidebar.width(), top:0};  
+        }
+    }
+
+    MobileMenu.prototype.positionSidebar = function($sidebar) {
+        console.log(this.isMobile);
+        if (this.isMobile) {
+           if (settings.sidebarLocation == "left") {
+                return {left:-$sidebar.width(), top:0}
+            } else {
+                return {right:$sidebar.width(), top:0}
+            }
+        } else {
+            if (settings.sidebarLocation == "left") {
+                console.log($sidebar.width());
+                return {left:0, top:0}
+            } else {
+                return {right:$sidebar.width(), top:0}
+            }
+        }
+    }
+
+    MobileMenu.prototype.animate = function($burger, $sidebar, $body) {
+        var _this = this;
+
+        $burger.click(function() {
+            $sidebar.animate(_this.positionSidebar($sidebar), settings.duration);
+            console.log(_this.positionSidebar($sidebar));
+            _this.toggleMobile();
+        })   
     }
 
     $.fn.mobilemenu = function(options) {
