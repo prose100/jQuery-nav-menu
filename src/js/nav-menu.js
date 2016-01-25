8cf99caf9b9d709c024d3d8df8792d39bf517710
Paul Rose <prose100@hotmail.com>
2016-01-25 13:15:31 -0600

    Removed animated burger. Improved up sidebar animation. Some code refactoring.

--- src/js/nav-menu.js
+++ src/js/nav-menu.js
@@ -1,10 +1,11 @@
 ;(function($){
 
     var defaults = {
-        windowWidth: "800",
+        windowWidth: "900",
         duration: 500,
-        sidebarLocation: 'right'
-    };
+        sidebarLocation: 'left',
+        slideDistance: "60"
+    }
 
     function MobileMenu (element, options) {
         settings = $.extend({}, defaults, options);
@@ -17,42 +18,54 @@
     MobileMenu.prototype.init = function() {
          document.body.style.overflow = 'hidden';
 
-        var $burger = this.createBurger();
-
-        this.initDisplay($burger);
-        this.resizeWindow($burger);
-        this.animateBurger($burger);
-
         var $sidebar = this.createSidebar();
-        var $body = $('<body>');
+        var $burger = this.createBurger($sidebar);
+        var $wrapper = this.createWrapper($burger, $sidebar);
 
-        this.animate($burger, $sidebar, $body);
+        this.initPositions($burger, $sidebar, $wrapper);
+        this.configDisplay($burger, $sidebar);
+        this.resizeWindow($burger, $sidebar, $wrapper);
+        
+        this.animate($burger, $sidebar, $wrapper);
     }
 
     MobileMenu.prototype.toggleMobile = function() {
         this.isMobile = !this.isMobile;
     }
 
-    MobileMenu.prototype.createBurger = function() {
-        var $burger = $('<div>').attr('id', 'burger').insertAfter($this);
+    MobileMenu.prototype.createBurger = function($sidebar) {
+        var $burger = $('<div>').attr('id', 'burger')
         var $span = $('<span>').appendTo($burger);
+         
+        this.absolute($burger);
 
         return $burger;
     }
 
-    MobileMenu.prototype.createSidebar = function() {
-        var $sidebar = $('<ul>').attr('id', 'sidebar').insertAfter($this);
+    MobileMenu.prototype.createSidebar = function($burger) {
+        var $sidebar = $('<ul>').attr('id', 'sidebar')
         var $sidebarList = $this.children().css({}).clone().appendTo($sidebar);
 
-        $sidebar.css({'position':'absolute', 'list-style':'none',
-                        'display':'inline-block', 'padding': 0});
-        $sidebar.css(this.positionInit($sidebar));
-        $sidebar.hide();
-        console.log(-$sidebar.width());
+        $sidebar.css({'list-style':'none', 'padding': 0});
+        
+        this.absolute($sidebar);
+    
         return $sidebar;
     }
 
-    MobileMenu.prototype.initDisplay = function($burger) {
+    MobileMenu.prototype.createWrapper = function($burger, $sidebar) {
+        var $wrapper = $('<div>').attr('id', 'wrapper').insertAfter($this);
+        
+        $burger.appendTo($wrapper);
+        $sidebar.appendTo($wrapper);
+
+        this.relative($wrapper);
+        $wrapper.addClass('clearfix');
+
+        return $wrapper;
+    }
+
+    MobileMenu.prototype.configDisplay = function($burger, $sidebar) {
         if ($(window).width()<settings.windowWidth) {
                 $burger.show();
                 $this.hide();
@@ -62,52 +75,58 @@
             }   
     }
 
-    MobileMenu.prototype.resizeWindow= function($burger) {
+    MobileMenu.prototype.absolute = function(absolute) {
+        absolute.css({'position':'absolute'});
+    }
+
+    MobileMenu.prototype.relative = function(relative) {
+        relative.css({'position':'relative'});
+    }
+
+    MobileMenu.prototype.resizeWindow = function($burger, $sidebar, $wrapper) {
         var _this = this;
 
         $(window).resize(function() {
-           _this.initDisplay($burger);
+           _this.initPositions($burger, $sidebar, $wrapper);
+
+           _this.configDisplay($burger, $sidebar);
         })
     }
-    
-    MobileMenu.prototype.animateBurger = function($burger) {
-        $burger.click(function() {
-            $(this).toggleClass('active');
-            });
-    }
 
-    MobileMenu.prototype.positionInit = function($sidebar) {
-        if (settings.sidebarLocation == "left") {
-            return {left:-$sidebar.width(), top:0};
-        } else {
-            return {right:-$sidebar.width(), top:0};  
+    MobileMenu.prototype.initPositions = function($burger, $sidebar, $wrapper) {
+         if (settings.sidebarLocation == "left") {
+            $burger.css({left:$sidebar.width(), top:0});
+            $sidebar.css({left:0, top:0});
+            $wrapper.css({left:-$sidebar.width()-8, top:0});
+        } else if (settings.sidebarLocation == "right") {
+            $burger.css({left:0, top:0});
+            $sidebar.css({left:$burger.width(), top:0});
+            $wrapper.css({left:($(window).width()-$burger.width())+"px", top:0})
         }
     }
 
-    MobileMenu.prototype.positionSidebar = function($sidebar) {
-        console.log(this.isMobile);
+    MobileMenu.prototype.nextPositionSidebar = function() {
         if (this.isMobile) {
-           if (settings.sidebarLocation == "left") {
-                return {left:-$sidebar.width(), top:0}
-            } else {
-                return {right:-$sidebar.width(), top:0}
+            if (settings.sidebarLocation == "left") {
+                return {left: "-=" + settings.slideDistance + "px"}
+            } else if (settings.sidebarLocation == "right") {
+                return {left: "+=" + settings.slideDistance + "px"}
             }
         } else {
             if (settings.sidebarLocation == "left") { 
-                return {left:0, top:0}
-            } else {
-                return {right:0, top:0}
+                return {left: "+=" + settings.slideDistance + "px"}
+            } else if (settings.sidebarLocation == "right") {
+                return {left: "-=" + settings.slideDistance + "px"}
             }
         }
     }
 
-    MobileMenu.prototype.animate = function($burger, $sidebar, $body) {
+    MobileMenu.prototype.animate = function($burger, $sidebar, $wrapper) {
         var _this = this;
 
         $burger.click(function() {
             $sidebar.show();
-            $sidebar.animate(_this.positionSidebar($sidebar), settings.duration);
-            console.log(_this.positionSidebar($sidebar));
+            $wrapper.animate(_this.nextPositionSidebar(), settings.duration);
             _this.toggleMobile();
         })   
     }
